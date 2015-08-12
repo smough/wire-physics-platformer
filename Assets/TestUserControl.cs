324671aa12421c463cef606311bae28c78d91819
fvescio <francesco.d.vescio@gmail.com>
2015-08-12 16:28:02 -0700

    First commit

---
+++ Assets/TestUserControl.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUserControl : MonoBehaviour {
+
+	[SerializeField] private float m_MaxSpeed = 10f;
+	[SerializeField] private float m_JumpForce = 500f;
+	[SerializeField] private LayerMask m_WhatIsGround;
+
+	private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool m_Grounded;            // Whether or not the player is grounded.
+	private bool m_FacingRight = true;
+	private Rigidbody2D m_Rigidbody2D;
+	//private LayerMask ground = "Default";
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 pos = (Vector2)gameObject.transform.position;
+
+		Collider2D[] colliders = Physics2D.OverlapCircleAll (pos, k_GroundedRadius);
+		foreach(Collider2D collider in colliders)
+		{
+			if(collider.gameObject != gameObject)
+			{
+				m_Grounded = true;
+			}
+		}
+
+		var move = Input.GetAxis ("Horizontal");
+
+		if (move > 0 && !m_FacingRight) {
+			Flip();
+		} else if (move < 0 && m_FacingRight) {
+			Flip();
+		}
+
+		m_Rigidbody2D.velocity  = new Vector2 (move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+
+		if (m_Grounded && Input.GetButton ("Jump"))
+		{
+			m_Grounded = false;
+			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+		}
+
+	}
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	public bool FacingRight()
+	{
+		return m_FacingRight;
+	}
+}
---
+++ Assets/Wire.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wire : MonoBehaviour 
+{
+	[SerializeField] private float m_WireForce = 70f;
+	[SerializeField] private float m_WireLength = 100f;
+	[SerializeField] private float m_FiringDelay = 300f;
+	[SerializeField] private LayerMask m_AttachesToWhat;
+
+	private Rigidbody2D m_RigidBody2D;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_RigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log (gameObject.GetComponent<TestUserControl>().FacingRight ());
+
+		if(Input.GetButton("Fire1"))
+		{
+			//Debug.Log("You pressed the right button!");
+			if(gameObject.GetComponent<TestUserControl>().FacingRight())
+			{
+				RaycastHit2D ray = Physics2D.Raycast((Vector2)transform.position, Vector2.right, m_WireLength, m_AttachesToWhat);
+				if(ray.collider != null)
+				{
+					//m_RigidBody2D.MovePosition(ray.point);
+					m_RigidBody2D.AddForce(new Vector2(m_WireForce, 0));
+					Debug.Log (m_RigidBody2D.position);
+					Debug.DrawLine(transform.position, (Vector3)ray.point);
+				}
+			}
+			else
+			{
+				RaycastHit2D ray = Physics2D.Raycast((Vector2)transform.position, Vector2.left, m_WireLength, m_AttachesToWhat);
+				if(ray.collider != null)
+				{
+					m_RigidBody2D.AddForce(new Vector2(-m_WireForce, 0));
+					Debug.Log (m_RigidBody2D.position);
+					Debug.DrawLine(transform.position, (Vector3)ray.point);
+				}
+			}
+		}
+	}
+}
